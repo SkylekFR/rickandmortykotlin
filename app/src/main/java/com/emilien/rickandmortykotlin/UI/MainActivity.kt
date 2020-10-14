765714ad4695bd3a964dc899665d628e57a4de2f
CHAMPION EMILIEN <emilien.champion@etu.univ-lyon1.fr>
2020-10-14 12:52:15 +0200

    wip

--- app/src/main/java/com/emilien/rickandmortykotlin/UI/MainActivity.kt
+++ app/src/main/java/com/emilien/rickandmortykotlin/UI/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.emilien.rickandmortykotlin.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -22,13 +23,18 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
     val RC_SIGN_IN = 1001
     val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-        .requestIdToken("215644384276-5aln613c6lkgnbe4v8d19a8ss5oqunhn.apps.googleusercontent.com")
+        .requestIdToken("215644384276-7rg4bb2ha8jre38hsj3s9gmt1m61747b.apps.googleusercontent.com")
         .requestEmail()
         .build()
 
     lateinit var gotoappBtn: Button
     lateinit var signinBtn: SignInButton
     lateinit var infoTv: TextView
+    lateinit var disconnectButton: Button
+    lateinit var registerButton: Button
+    lateinit var emailTextView: TextView
+    lateinit var passwordTextView: TextView
+    lateinit var connectButton: Button
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,8 +43,14 @@ class MainActivity : AppCompatActivity() {
         gotoappBtn = findViewById(R.id.activity_main_gotoappBtn)
         signinBtn = findViewById(R.id.activity_main_signinBtn)
         infoTv = findViewById(R.id.activity_main_infoTv)
-
+        disconnectButton = findViewById(R.id.activity_main_disconnectButton)
+        registerButton = findViewById(R.id.activity_main_registerButton)
+        emailTextView = findViewById(R.id.activity_main_emailInputTextView)
+        passwordTextView = findViewById(R.id.activity_main_passwordInputTextView)
+        connectButton = findViewById(R.id.activity_main_connectButton)
         signinBtn.setOnClickListener(clickListener)
+        gotoappBtn.visibility = View.GONE
+        gotoappBtn.text = "Start"
         gotoappBtn.setOnClickListener(clickListener)
 
     }
@@ -100,12 +112,13 @@ class MainActivity : AppCompatActivity() {
         if (currentUser == null) {
            infoTv.setText("You need to connect to use the app")
             signinBtn.visibility = View.VISIBLE
-            //gotoappBtn.visibility = View.GONE
+            gotoappBtn.visibility = View.GONE
+
         }
         else{
             infoTv.setText("Welcome ${auth.currentUser?.email}")
             signinBtn.visibility = View.GONE
-            //gotoappBtn.visibility = View.VISIBLE
+            gotoappBtn.visibility = View.VISIBLE
         }
     }
     val clickListener = View.OnClickListener { view ->
@@ -113,12 +126,62 @@ class MainActivity : AppCompatActivity() {
         when (view.getId()) {
             R.id.activity_main_gotoappBtn -> startCharacterListActivity()
             R.id.activity_main_signinBtn -> signIn()
+            R.id.activity_main_disconnectButton -> disconnect()
+            R.id.activity_main_connectButton -> connect()
+            R.id.activity_main_registerButton -> register("c'est en dur", "c dur")
         }
     }
 
-    fun startCharacterListActivity() {
+    private fun startCharacterListActivity() {
         val intent = Intent(this, CharacterListActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun disconnect() {
+            FirebaseAuth.getInstance().signOut()
+            updateUI(null);
+    }
+    private fun connect() {
+        auth.signInWithEmailAndPassword(emailTextView.text.toString(), passwordTextView.text.toString()).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d(TAG, "connectedWithEmail:success")
+                val user = auth.currentUser
+                updateUI(user)
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                Toast.makeText(baseContext, "Authentication failed.",
+                    Toast.LENGTH_SHORT).show()
+                updateUI(null)
+            }
+
+            // ...
+        }
+
+    }
+
+    private fun register(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
+                // ...
+            }
+    }
+    companion object {
+        private const val TAG = "MainActivity"
     }
 
 
